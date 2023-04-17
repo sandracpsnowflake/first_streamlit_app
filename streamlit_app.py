4343e7dfeee6acc516a39cac8a45d3c1d5368f5e
sandracpsnowflake <130797554+sandracpsnowflake@users.noreply.github.com>
2023-04-17 17:18:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def get_fruitvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 
-def get_fruit_load_list()
+def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM fruit_load_list")
     return my_cur.fetchall()
